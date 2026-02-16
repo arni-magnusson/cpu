309ff63e93739f352ca6ee43761e19ba0bf816ee
Arni Magnusson <thisisarni@gmail.com>
2026-02-16 17:42:03 +1100

    Add output.R

--- output.R
+++ output.R
@@ -1,9 +1,12 @@
 # Extract results of interest, write CSV output tables
 
-# Before:
-# After:
+# Before: i7_high.csv, i7_low.csv, i7_medium.csv, r7_high.csv,
+#         r7_low.csv (model)
+# After:  i7_high.csv, i7_low.csv, i7_medium.csv, r7_high.csv,
+#         r7_low.csv (output)
 
 library(TAF)
 
 mkdir("output")
 
+cp("model/*.csv", "output")
