7b7f3ec5117b7e7b5c645d619ed778a048fcb4f2
Arni Magnusson <thisisarni@gmail.com>
2026-07-16 15:12:11 +1100

    Calculate smooth trend

--- turbostat/model.R
+++ turbostat/model.R
@@ -19,54 +19,28 @@ p3560 <- readRDS("data/p3560.rds")
 
 taf.png("model/m4700_Repairs")
 par(mfrow=c(1, 4))
-# Repair m4700
-x <- m4700$full$Watt
-m4700$full$Watt <- outliers(x, level=0.4, span=10, plot=TRUE)$repaired
-x <- m4700$main$Watt
-m4700$main$Watt <- outliers(x, level=0.3, span=10, plot=TRUE)$repaired
-x <- m4700$single$Watt
-m4700$single$Watt <- outliers(x, span=10, plot=TRUE)$repaired
 x <- m4700$idle$Watt
-m4700$idle$Watt <- outliers(x, level=0.07, plot=TRUE)$repaired
+m4700$idle$Watt <- outliers(x, dist=0.5, plot=TRUE)$repaired
 dev.off()
 
 taf.png("model/p1gen8_Repairs")
 par(mfrow=c(1, 4))
-# Repair p1gen8
-x <- p1gen8$full$Watt
-p1gen8$full$Watt <- outliers(x, span=0.43, plot=TRUE)$repaired
 x <- p1gen8$main$Watt
-p1gen8$main$Watt <- outliers(x, span=0.85, plot=TRUE)$repaired
-x <- p1gen8$single$Watt
-p1gen8$single$Watt <- outliers(x, span=0.85, plot=TRUE)$repaired
-x <- p1gen8$idle$Watt
-p1gen8$idle$Watt <- outliers(x, plot=TRUE)$repaired
+p1gen8$main$Watt <- outliers(x, dist=20, plot=TRUE)$repaired
 dev.off()
 
 taf.png("model/p15gen1_Repairs")
 par(mfrow=c(1, 4))
-# Repair p15gen1
-x <- p15gen1$full$Watt
-p15gen1$full$Watt <- outliers(x, level=0.3, span=0.5, plot=TRUE)$repaired
-x <- p15gen1$main$Watt
-p15gen1$main$Watt <- outliers(x, span=0.5, plot=TRUE)$repaired
-x <- p15gen1$single$Watt
-p15gen1$single$Watt <- outliers(x, level=0.3, span=0.8, plot=TRUE)$repaired
 x <- p15gen1$idle$Watt
-p15gen1$idle$Watt <- outliers(x, level=0.1, span=0.8, plot=TRUE)$repaired
+p15gen1$idle$Watt <- outliers(x, dist=0.2, plot=TRUE)$repaired
 dev.off()
 
 taf.png("model/p3560_Repairs")
 par(mfrow=c(1, 4))
-# Repair p3560
-x <- p3560$full$Watt
-p3560$full$Watt <- outliers(x, span=0.3, plot=TRUE)$repaired
-x <- p3560$main$Watt
-p3560$main$Watt <- outliers(x, span=0.25, plot=TRUE)$repaired
 x <- p3560$single$Watt
-p3560$single$Watt <- outliers(x, level=0.04, span=0.65, plot=TRUE)$repaired
+p3560$single$Watt <- outliers(x, dist=0.5, span=0.3, plot=TRUE)$repaired
 x <- p3560$idle$Watt
-p3560$idle$Watt <- outliers(x, level=0.7, span=0.65, plot=TRUE)$repaired
+p3560$idle$Watt <- outliers(x, dist=5, plot=TRUE)$repaired
 dev.off()
 
 # Save RDS objects
--- turbostat/report.R
+++ turbostat/report.R
@@ -17,6 +17,37 @@ p1gen8 <- readRDS("model/p1gen8.rds")
 p15gen1 <- readRDS("model/p15gen1.rds")
 p3560 <- readRDS("model/p3560.rds")
 
+iter <- seq(1:nrow(m4700$idle))
+
+# Smooth m4700
+smooth_m4700 <- m4700
+smooth_m4700$full$Watt <- mean(m4700$full$Watt)
+smooth_m4700$main$Watt <- mean(m4700$main$Watt)
+smooth_m4700$single$Watt <- mean(m4700$single$Watt)
+smooth_m4700$idle$Watt <- mean(m4700$idle$Watt)
+
+# Smooth p1gen8
+smooth_p1gen8 <- p1gen8
+smooth_p1gen8$full$Watt <- loess(Watt~iter, p1gen8$full, span=0.4)$fitted
+smooth_p1gen8$main$Watt <- loess(Watt~iter, p1gen8$main, span=0.4)$fitted
+smooth_p1gen8$single$Watt <- loess(Watt~iter, p1gen8$single, span=0.6)$fitted
+smooth_p1gen8$idle$Watt <- mean(p1gen8$idle$Watt)
+
+# Smooth p15gen1
+smooth_p15gen1 <- p15gen1
+smooth_p15gen1$full$Watt <- loess(Watt~iter, p15gen1$full, span=0.9)$fitted
+smooth_p15gen1$main$Watt <- loess(Watt~iter, p15gen1$main, span=0.9)$fitted
+smooth_p15gen1$single$Watt <- loess(Watt~iter, p15gen1$single, span=0.9)$fitted
+mean(p15gen1$idle$Watt)
+
+# Smooth p3560
+smooth_p3560 <- p3560
+smooth_p3560$full$Watt <- loess(Watt~iter, p3560$full, span=0.11)$fitted
+smooth_p3560$main$Watt <- loess(Watt~iter, p3560$main, span=0.21)$fitted
+stable <- replace(p3560$single$Watt, 120:300, mean(p3560$single$Watt[120:300]))
+smooth_p3560$single$Watt <- loess(stable~iter, span=0.30)$fitted
+smooth_p3560$idle <- mean(p3560$idle$Watt)
+
 # Plot m4700
 taf.png("m4700_Temp")
 plot_machine(m4700, "Temp")
