e376d3658ecbae4aaaa30805668fef96eef5611d
Arni Magnusson <thisisarni@gmail.com>
2026-08-04 10:51:47 +1100

    Comment header

--- powerstat/data.R
+++ powerstat/data.R
@@ -1,7 +1,9 @@
 # Prepare data, write CSV data tables
 
-# Before:
-# After:
+# Before: info.csv (boot/data)
+#         power_*.dat (boot/data/lenovo_p15_gen_1)
+# After:  info.csv,
+#         p15gen1_*.csv, p15gen1.rds (data)
 
 library(TAF)
 
